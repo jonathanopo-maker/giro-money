7e27e4ac2b6d4b3b4d5498b4254e040dffc8df77
Jonathanopo-maker <jhonathan.opo@gmail.com>
2026-04-13 15:32:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-// ⚠️ depois você vai trocar pelo Mongo Atlas
+//
 mongoose.connect("mongodb://127.0.0.1:27017/giro");
 
 // modelos
